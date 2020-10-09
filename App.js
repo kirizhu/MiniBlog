a13f3167eb29111b4564ca5daf38a5991235178d
KiriZhu <kiribaty88@gmail.com>
2020-10-09 15:08:22 +0200

    installed navigation and created stacknavigation and screens

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import IndexScreen from './src/screens/IndexScreen';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerTitleAlign: 'center' }}
+        initialRouteName='Index'
+      >
+        <Stack.Screen
+          name='Index'
+          component={IndexScreen}
+          options={{ title: 'Mini Blog' }}
+        />
+        <Stack.Screen
+          name='Show'
+          component={ShowScreen}
+          options={{ title: 'Show Blog' }}
+        />
+        <Stack.Screen
+          name='Create'
+          component={CreateScreen}
+          options={{ title: 'Create Blog' }}
+        />
+        <Stack.Screen
+          name='Edit'
+          component={EditScreen}
+          options={{ title: 'Edit Blog' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
